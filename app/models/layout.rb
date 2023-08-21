d17b480e26cff6bac570d80613df7a30c8a73a93
Mana Misawa <130806211+mnsom@users.noreply.github.com>
2023-08-21 15:37:16 +0900

    Revert "models"

--- app/models/layout.rb
+++
@@ -1,5 +0,0 @@
-class Layout < ApplicationRecord
-  belongs_to :user
-  has_many :registered_items
-  validates :title, :scale_ratio, presence: true
-end
--- app/models/registered_item.rb
+++
@@ -1,5 +0,0 @@
-class RegisteredItem < ApplicationRecord
-  belongs_to :item
-  belongs_to :layout
-  validates :x, :y, :rotation, :length, :width, presence: true
-end
