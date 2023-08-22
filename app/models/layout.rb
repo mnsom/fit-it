6727769e9c3d749a8d9e0da9b23acf4a3baee357
Mana Misawa <mnmisawa04@gmail.com>
2023-08-22 15:43:01 +0900

    Fixed new page

--- app/models/layout.rb
+++ app/models/layout.rb
@@ -1,6 +1,6 @@
 class Layout < ApplicationRecord
   belongs_to :user
   has_many :registered_items
-  validates :title, :scale_ratio, presence: true
+  validates :title, presence: true
   has_one_attached :photo, dependent: :destroy
 end
