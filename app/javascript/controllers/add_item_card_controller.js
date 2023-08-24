ead124c59454cca795ea7b41c54f8978d18daf96
Mana Misawa <mnmisawa04@gmail.com>
2023-08-24 12:05:08 +0900

    Fixed the add-item form

--- app/javascript/controllers/add_item_card_controller.js
+++ app/javascript/controllers/add_item_card_controller.js
@@ -2,11 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="add-item-card"
 export default class extends Controller {
+static values = {
+  popup: Boolean
+}
 
-static targets=["popup"]
+static targets=["popup", "modal"]
 
   connect() {
     console.log("connected")
+    if (this.popupValue){
+      this.popupTarget.classList.remove("d-none")
+      const myModal = new bootstrap.Modal(this.modalTarget)
+      // const modalToggle = document.getElementById('toggleMyModal');
+      myModal.show(this.modalTarget)
+    }
   }
 
   toggle() {
