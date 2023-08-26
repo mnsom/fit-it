58e177af71078ebc78a92b7a14b7ea9e976c13ef
Mana Misawa <mnmisawa04@gmail.com>
2023-08-26 13:12:30 +0900

    Layout-preview function added

---
+++ app/javascript/controllers/photo_preview_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="photo-preview"
+export default class extends Controller {
+  connect() {
+    console.log("connected!");
+  }
+
+  static targets = ["input", "preview"]
+
+  displayPreview(event) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      this.previewTarget.src = event.currentTarget.result;
+    }
+    reader.readAsDataURL(this.inputTarget.files[0])
+    this.previewTarget.classList.remove('hidden');
+  }
+}
