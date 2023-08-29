bf1fac34fd09f309d3268da457f23e71a3f12090
Mana Misawa <mnmisawa04@gmail.com>
2023-08-29 12:55:23 +0900

    Fixed meta tags

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,15 +1,15 @@
 module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["Fit It - Clean layout Clear mind"]
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
   end
 
   def meta_description
-    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["Visualize a better layout and decide the location of furniture for your new home"]
+    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
   end
 
   def meta_image
-    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["LandingPageBg.svg"])
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
     # little twist to make it work equally with an asset or a url
-    # meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
+    meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
   end
 end
